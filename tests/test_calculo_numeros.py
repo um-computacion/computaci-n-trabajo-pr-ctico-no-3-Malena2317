08d3694f5bdf4b62d47136951a49ab7cd9d8347a
Malena2317 <me.navarro@alumno.um.edu.ar>
2025-04-14 21:50:09 -0300

    Agregar tests para ingreso de números negativos

--- tests/test_calculo_numeros.py
+++ tests/test_calculo_numeros.py
@@ -10,8 +10,7 @@ class TestCalculoNumeros(unittest.TestCase):
 
     @patch(  # este patch controla lo que hace el input
         'builtins.input',
-        return_value='100'
-    )
+        return_value='100')
     def test_ingreso_feliz(self, patch_input):
         numero = ingrese_numero()
         self.assertEqual(numero, 100)
@@ -25,6 +24,24 @@ class TestCalculoNumeros(unittest.TestCase):
     def test_ingreso_feliz_2000(self, patch_input):
         numero = ingrese_numero()
         self.assertEqual(numero, 2000)
+    
+    @patch(  # este patch controla lo que hace el input
+        'builtins.input',
+        return_value='-100'
+    )
+    def test_ingreso_negativo(self, patch_input):
+        with self.assertRaises(NumeroDebeSerPositivo):
+            ingrese_numero()
 
+    @patch('builtins.input',return_value='-1')
+    def test_ingreso_negativo_uno(self, patch_input):
+        with self.assertRaises(NumeroDebeSerPositivo):
+            ingrese_numero()
+
+    @patch('builtins.input',return_value='-9999')
+    def test_ingreso_negativo_grande(self, patch_input):
+        with self.assertRaises(NumeroDebeSerPositivo):
+            ingrese_numero()
+            
 if __name__ == '__main__':
     unittest.main() 
